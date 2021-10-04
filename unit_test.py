da48d96139e207c5c560216bc2774f6a29fd52a7
aayu-s <ayuships33@gmail.com>
2021-10-04 11:28:25 +0530

    Add files via upload

--- unit_test.py
+++ unit_test.py
@@ -1,8 +1,6 @@
-import os
-from settings import PROJECT_ROOT
 import unittest
 
-from route import Route_for_A, Route_for_B
+from route import Route
 from services import Services
 
 
@@ -11,21 +9,21 @@ class TestCases(unittest.TestCase):
         input_file = '/home/ayushi/myprojects/train/input.txt'
         self.assertEqual(Services.check_valid_file(input_file), True)
 
-    def test_invalid_file(self):
-        input_file = '/home/ayushi/myprojects/train/input.txt'
-        self.assertEqual(Services.check_valid_file(input_file), False)
-
     def test_valid_station(self):
+        Route.register_routes()
         input_file = '/home/ayushi/myprojects/train/input.txt'
         trains = Services.read_file(input_file)
-        new_stations = Services.print_path_till_hyb(trains)
-        self.assertEqual(type([]), type(new_stations))
+        for train in trains:
+            value = Services.get_bogie_at('HYB', train)
+            self.assertEqual(type([]), type(value))
 
     def test_invalid_station(self):
+        Route.register_routes()
         input_file = '/home/ayushi/myprojects/train/input.txt'
         trains = Services.read_file(input_file)
-        msg = Services.print_path_till_hyb(trains)
-        self.assertEqual('Invalid Station', msg)
+        for train in trains:
+            msg = Services.get_bogie_at('PPP', train)
+            self.assertEqual('E01', msg)
 
 
 if __name__ == '__main__':
